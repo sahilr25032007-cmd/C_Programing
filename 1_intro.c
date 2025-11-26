d8c11384fd864772eecf3b5f7a88336afb22f7d6
sahilr25032007-cmd <sahilr25032007@gmail.com>
2025-11-26 21:13:42 +0530

    Add files via upload

--- 1_intro.c
+++ 1_intro.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
 
 int main(void){
 
-    int array[3][3];
+    float* numbers = calloc(10,sizeof(int));
+    for(int i = 0; i < 10; ++i){
+        printf("%d ",numbers[i]);
+    }
 
-    for (int i = 0; i < 3; i++)
-    {
-        for(int j = 0; j < 3; ++j){
-            printf("enter a number:");
-            scanf("%d",&array[i][j]);
-        }
-    }
-    for (int i = 0; i < 3; i++)
-    {
-        for(int j = 0; j < 3; ++j){
-            printf("%d ",array[i][j]);
-        }
-        printf("\n");
-    }
+    char* name = malloc(sizeof(char)*100);
+    printf("\nEnter your name: ");
+    scanf("%s",name);
+    
+    name = realloc(name,sizeof(int)*strlen(name)+1);
+
+    printf("%s",name);
+
+    free(numbers);
+    free(name);
 
 }
